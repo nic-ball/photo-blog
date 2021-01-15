cd4e0fe9dd7e5d91e3498f16bf27916927cc511c
nic-ball <nicholasball147@gmail.com>
2021-01-15 10:19:39 +0000

    Linter config

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
-app.listen(5000)
+app.listen(5000);
